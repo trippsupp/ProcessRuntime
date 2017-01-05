2d757494c0cf2c94f053139b010ee8cc692d76d9
trippsupp <trippsupp@gmail.com>
2017-01-05 02:25:25 -0500

    Initial commit.

---
+++ src/burt/Burt.java
@@ -0,0 +1,27 @@
+package burt;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Vance Field III
+ */
+public class Burt {
+       
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        JFrame frame = new JFrame("Shutdown");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setContentPane(new GUI());        
+        frame.setPreferredSize(new Dimension(300, 150));
+        frame.pack();                      // always pack the frame 
+        frame.setLocationRelativeTo(null); // sets app to middle of the screen
+        frame.setVisible(true);
+    }
+    
+}
